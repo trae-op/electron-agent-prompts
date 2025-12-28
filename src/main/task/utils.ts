5f54c1fad51d1e4d5c07665163b2648251f94675
oleg.pronchenko <traeop@gmail.com>
2025-12-28 12:26:23 +0200

    refactor(Task): enhance buildMarkdownDocument for code items

--- src/main/task/utils.ts
+++ src/main/task/utils.ts
@@ -1,6 +1,13 @@
 export function buildMarkdownDocument(contents: TMarkdownContent[]): string {
   return [...contents]
     .sort((a, b) => a.position - b.position)
-    .map((item) => item.content)
+    .map((item) => {
+      if (item.type === "code") {
+        const normalizedContent = item.content.replace(/\s+$/, "");
+        return ["```", normalizedContent, "```"].join("\n");
+      }
+
+      return item.content;
+    })
     .join("\n\n");
 }
