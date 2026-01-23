9af695625e9f46415eb85df6349fd6a2c9431bfb
oleg.pronchenko <traeop@gmail.com>
2026-01-23 14:08:26 +0200

    chore(package): update version to 0.21.46
    Bump the version number in package.json to reflect the latest
    release. This change ensures that the package version is
    accurate for distribution and dependency management.

--- src/main/auth/window.ts
+++ src/main/auth/window.ts
@@ -1,11 +1,9 @@
-import { BrowserWindow, session } from "electron";
+import { BrowserWindow } from "electron";
 import { createWindow } from "../@shared/control-window/create.js";
 import { restApi } from "../config.js";
 import { TLessProviders } from "./types.js";
 
 export function openWindow(provider: keyof TLessProviders): BrowserWindow {
-  const authSession = session.fromPartition("persist:auth");
-
   return createWindow<TWindows["auth"]>({
     hash: "window:auth",
     loadURL: `${restApi.urls.base}${restApi.urls.baseApi}${restApi.urls.auth.base}${restApi.urls.auth[provider]}`,
@@ -17,7 +15,7 @@ export function openWindow(provider: keyof TLessProviders): BrowserWindow {
       width: 400,
       height: 400,
       webPreferences: {
-        session: authSession,
+        partition: "persist:auth",
       },
     },
   });
